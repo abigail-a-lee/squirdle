ca8415e5caadc9f4cd54bc7d47f2f4679e670ad5
abigail-a-lee <aabigaillee@gmail.com>
2023-01-28 04:49:43 +0000

    added typing animation

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,17 +1,26 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { AppContext } from "../App";
 
 function Input({ inputPos, attempt }) {
   const { board, correctWord, attemptNumber, usedLetters, setUsedLetters } =
     useContext(AppContext);
   const input = board[attempt][inputPos];
+  const box = useRef(null);
+  if (input.length > 0 && box.current !== null) {
+    box.current.classList.add("gelatine");
+    box.current.classList.add("ring-1");
+  }
+  if (input.length === 0 && box.current !== null) {
+    box.current.classList.remove("gelatine");
+    box.current.classList.remove("ring-1");
+  }
 
   const green = correctWord.toUpperCase()[inputPos] === input;
   const yellow = !green && input !== "" && correctWord.includes(input);
 
   const letterCorrect =
     attemptNumber.attempt > attempt &&
-    (green ? "bg-green-500" : yellow ? "bg-yellow-500" : "bg-neutral-800");
+    (green ? "bg-green-500 flip" : yellow ? "bg-yellow-500" : "bg-neutral-800");
 
   useEffect(() => {
     if (input !== "" && !green && !yellow) {
@@ -19,7 +28,12 @@ function Input({ inputPos, attempt }) {
     }
   }, [attemptNumber.attempt]);
 
-  return <div className={`input ${letterCorrect}`}> {input}</div>;
+  return (
+    <div className={`input ${letterCorrect}`} ref={box}>
+      {" "}
+      {input}
+    </div>
+  );
 }
 
 export default Input;
